aead971a85ab0ecf42f4947fbba025dd84586cb9
Wouter Horré <wouter@zenjoy.be>
2018-09-24 09:31:56 +0200

    Add config command and change default build config location

---
+++ src/commands/config.ts
@@ -0,0 +1,29 @@
+import { Command } from '@oclif/command';
+import chalk from 'chalk';
+import buildConfig = require('../config/config');
+import nimbuConfig from '../nimbu/config';
+
+export default class Config extends Command {
+
+  static description = "Show resolved configuration";
+
+  async showBuildConfiguration() {
+    this.log(chalk.red("Build configuration"));
+    for(const key in buildConfig) {
+      this.log(`${key}: ${buildConfig[key]}`);
+    }
+  }
+
+  async showToolchainConfiguration() {
+    this.log(chalk.red("Toolchain configuration"));
+    this.log(`site: ${nimbuConfig.site}`);
+    this.log(`theme: ${nimbuConfig.theme}`);
+    this.log(`host: ${nimbuConfig.host}`);
+    this.log(`admin domain: ${nimbuConfig.adminHost}`);
+  }
+
+  async run() {
+    this.showBuildConfiguration();
+    this.showToolchainConfiguration();
+  }
+}
--- src/config/config.js
+++ src/config/config.js
@@ -1,18 +1,29 @@
-const paths = require('./paths.js')
+const paths = require('./paths.js');
 const path = require('path');
 const fs = require('fs');
+
+const projectPackageJson = require(path.resolve(
+  paths.PROJECT_DIRECTORY,
+  'package.json'
+));
 
 const defaultConfig = {
   REACT: false,
   CDN_ROOT: '../',
 };
 
-const projectConfigPath = path.resolve(
-  paths.PROJECT_DIRECTORY,
-  'scripts',
-  'config.js'
-);
+let projectConfigPath;
+if (projectPackageJson.nimbu && projectPackageJson.nimbu.config) {
+  projectConfigPath = path.resolve(
+    paths.PROJECT_DIRECTORY,
+    projectPackageJson.nimbu.config
+  );
+} else {
+  projectConfigPath = path.resolve(paths.PROJECT_DIRECTORY, 'nimbu.js');
+}
 
-const projectConfig = fs.existsSync(projectConfigPath) ? require(projectConfigPath) : {};
+const projectConfig = fs.existsSync(projectConfigPath)
+  ? require(projectConfigPath)
+  : {};
 
 module.exports = Object.assign({}, defaultConfig, projectConfig);
