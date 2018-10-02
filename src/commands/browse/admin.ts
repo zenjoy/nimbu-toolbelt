4c919bc3b121e501e20428929ea4bfa0032c881a
Wouter Horré <wouter@zenjoy.be>
2018-10-02 09:50:05 +0200

    Use cli-us for opening browser in browse:* commands

--- src/commands/browse/admin.ts
+++ src/commands/browse/admin.ts
@@ -1,12 +1,12 @@
 import Command from '../base';
 import Config from '../../nimbu/config';
-const openBrowser = require('react-dev-utils/openBrowser');
+import cli from 'cli-ux';
 
 export default class BrowseAdmin extends Command {
 
   static description = "open the admin area for your current site";
 
   async run() {
-    openBrowser(`https://${Config.site}.${Config.adminHost}/admin`);
+    cli.open(`https://${Config.site}.${Config.adminHost}/admin`);
   }
 }
--- src/commands/browse/simulator.ts
+++ src/commands/browse/simulator.ts
@@ -1,10 +1,10 @@
 import Command from '../base';
-const openBrowser = require('react-dev-utils/openBrowser');
+import cli from 'cli-ux';
 
 export default class BrowseSimulator extends Command {
   static description = "open the simulator for your current site"
 
   async run() {
-    openBrowser('http://localhost:4567/');
+    cli.open('http://localhost:4567/');
   }
 }
